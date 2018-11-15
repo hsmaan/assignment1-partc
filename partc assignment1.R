fc23569b36cbaae9da11a1185d04083badddec3f
H Maan <hassaanmaan@hotmail.com>
2018-11-14 19:11:58 -0500

    Added ablines and conclusions
    Created and added linear regression lines for the mean-variance plots to
    compare slope to that from expected Poisson model. Used different colors
    to represent both lines and added some general conclusions based on
    plots.

--- partc assignment1.R
+++ partc assignment1.R
@@ -1,4 +1,4 @@
-# In this analysis, the pickrell1 rna-seq dataset from a study on RNA sequencing by Joseph Pickrell et al. The dataset will be analyzed based on stratification by gender, and an analysis of the relationship between mean and variance for expression will also be conducted. The distribution of the mean and variance for expression data will be compared to the poisson distribution. 
+# In this analysis, the pickrell1 rna-seq dataset from a study on RNA sequencing by Joseph Pickrell et al. The dataset will be analyzed based on stratification by gender, and an analysis of the relationship between mean and variance for expression will also be conducted. The distribution of the mean and variance for expression data will be compared to the Poisson distribution. 
 
 # We can begin by loading the dataset and some necessary libraries.
 
@@ -46,15 +46,23 @@ y <- y[isexpr & hasannot, , keep.lib.sizes=FALSE]
 # Check libsize for the two genders using a boxplot 
 boxplot(split(log10(y$samples$lib.size),Gender),main="Gender Based Library Size Comparison for Pickrell1", ylab="log10 library size")
 
-# Compare mean-var relationship at gene-level relative to Poisson
+# We want to compare mean-var relationship at gene-level relative to Poisson
+# First lets get the mean and coefficient of variation values 
 gene.mean <- apply(y$count,1,mean)
 CV.sq <- apply(y$count,1,var)/gene.mean^2
 
-# Plot CV
+# Plot log of mean and coefficient of variation for the data
 plot(log(gene.mean), log(CV.sq),cex=0.3,col="grey",ylim=c(-5,3), main="Mean-Variance Relationship for Pickrell1 Dataset Compared to Poisson")
 
-# Add predicted CV.sq under Poisson (do you know why under Poisson model the slope is -1?
-abline(c(0,-1))
+# Add linear regression line for the mean-var relationship
+abline(lm(log(gene.mean)~log(CV.sq)),col='red')
+
+# Add predicted regression line under Poisson 
+abline(c(0,-1),col='blue')
+
+# The red line indicates the regression line for the actual dataset before any normalization, and the blue line indicates the expected regression slope under the Poisson distribution. Therefore, we can see that the relationship between the mean and the variance for this dataset follows very closely to the expected relationship under the Poisson distribution.
+
+# Now we can compare the mean-var relationship to Poisson the same way after normalization of the expression counts. 
 
 # First calculate relative NORMALIZATION factors (using default method)
 y <- calcNormFactors(y)
@@ -64,12 +72,17 @@ sf <- colSums(y$count); sf <- sf/mean(sf)
 eff.sf <- sf*y$samples$norm.factors
 norm.y <- y$count %*% diag(1/eff.sf)
 
-# Compare mean-var relationship at gene-level relative to Poisson
+# Again compare mean-var relationship at gene-level relative to Poisson
 gene.mean <- apply(norm.y,1,mean)
 CV.sq <- apply(norm.y,1,var)/gene.mean^2
 plot(log(gene.mean), log(CV.sq),cex=0.3,col="grey",ylim=c(-5,3), main="Normalized Mean-Variance Relationship for Pickrell1 Dataset Compared to Poisson")
 
-# Add predicted CV.sq under Poisson 
-abline(c(0,-1))
+# Add linear regression line for the mean-var relationship
+abline(lm(log(gene.mean)~log(CV.sq)), col='red')
+
+# Add predicted regression line under Poisson 
+abline(c(0,-1),col='blue')
+
+# There is quite a difference in the slope of the actual regression line for the mean-var relationship after normalization of the read counts. The slope (from the red regression line), seems to increase as compared to before normalization, and is therefore not as well represented by the Poisson model anymore. 
 
 ##Any conclusions?
